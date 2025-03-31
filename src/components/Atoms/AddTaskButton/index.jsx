4649f8e89d95eb744e4629f1fb0173aac545b1d4
KugaChihiro <c.kuga.24.4.1410@4nonome.com>
2025-03-31 23:52:46 +0900

    AddTaskButtonの作成

---
+++ src/components/Atoms/AddTaskButton/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import COLOR from "../../../variables/color";
+import Plus from "../../../assets/svg/plus.svg";
+import FONTFAMILY from "../../../variables/font_family";
+import TEXT from "../../../variables/texts";
+
+export const AddTaskButton = () => {
+  return (
+    <StyledButton>
+      <img src = {Plus}/>
+      <StyledText>タスクを追加</StyledText>
+    </StyledButton>
+  )
+}
+
+const StyledButton = styled.button`
+  display:flex;
+  gap:10px;
+  margin:0;
+  padding: 2px 6px;
+  align-items:center;
+  color:${COLOR.GREEN};
+  font-family: ${FONTFAMILY.NOTO_SANS};
+  background: transparent;
+  border:none;
+  ${TEXT.S}
+
+  &:hover {
+    background-color:${COLOR.GREEN_TRANSLUCENT};
+    border-radius: 12%;
+  }
+`
+
+const StyledText = styled.p`
+  margin:0;
+  padding:0;
+`
---
+++ src/components/Atoms/AddTaskButton/index.stories.jsx
@@ -0,0 +1,8 @@
+import { AddTaskButton } from "./index";
+export default { component: AddTaskButton };
+
+export const Default = {
+  args: {
+    onClick: () => console.log("clicked"),
+  },
+};
