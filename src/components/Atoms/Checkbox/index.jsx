12edda09aad49806788a1f8e77fc4a6043ef6885
KugaChihiro <c.kuga.24.4.1410@4nonome.com>
2025-04-02 11:11:33 +0900

    Checkboxの作成

---
+++ src/components/Atoms/Checkbox/index.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import COLOR from "../../../variables/color";
+import Check from "../../../assets/svg/check.svg";
+
+export const Checkbox = () => {
+  return (
+    <StyledButton>
+      <StyledImg src = {Check}/>
+    </StyledButton>
+  )
+}
+
+const StyledButton = styled.button`
+  width:20px;
+  height:20px;
+  background:transparent;
+  border-radius:2px;
+  border:2px solid ${COLOR.LIGHT_GRAY_TRANSLUCENT_2};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const StyledImg = styled.img`
+  width:16px;
+  height:16px;
+  display:none;
+
+  ${StyledButton}:hover & {
+    display: inline;
+  }
+`
