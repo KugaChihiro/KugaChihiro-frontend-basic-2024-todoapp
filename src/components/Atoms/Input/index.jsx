c1c204dc1b46376e0917ecb147d13426238d507d
KugaChihiro <c.kuga.24.4.1410@4nonome.com>
2025-04-04 17:49:05 +0900

    Inputの作成

---
+++ src/components/Atoms/Input/index.jsx
@@ -0,0 +1,62 @@
+import React, { useRef, useEffect } from 'react';
+import styled from 'styled-components';
+import COLOR from "../../../variables/color";
+import FONTFAMILY from "../../../variables/font_family";
+import TEXT from "../../../variables/texts";
+
+export default function Input ({ onEditComplete, defaultValue }) {
+
+  const inputRef = useRef();
+
+  const handleBlur = () => {
+    const text = inputRef.current.value;
+    onEditComplete(text);
+  }
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      const text = inputRef.current.value;
+      onEditComplete(text);
+    }
+  }
+
+  useEffect(() => {
+    console.log("initialized");
+    inputRef.current.focus();
+  }, [])
+
+  return (
+    <StyledWrapper>
+      <StyledText defaultValue={defaultValue} ref={inputRef} onBlur={handleBlur} onKeyDown={handleKeyDown}>
+      </StyledText>
+    </StyledWrapper>
+  )
+
+}
+
+const StyledWrapper = styled.div`
+  background-color:${COLOR.BLACK};
+  width:100%;
+  padding:0px 4px;
+  border-radius:2px;
+  border:none;
+  display:flex;
+  align-items:center;
+`
+const StyledText= styled.input`
+  width:auto;
+  height:100%;
+  background:transparent;
+  caret-color: ${COLOR.LIGHT_GRAY_TRANSLUCENT_2};
+  border:none;
+  font-family:${FONTFAMILY.NOTO_SANS};
+  ${TEXT.S}
+  font-weight:500;
+  color:${COLOR.LIGHT_GRAY_TRANSLUCENT_2};
+
+  &:focus {
+    outline: none;
+    border: none;
+  }
+
+`
