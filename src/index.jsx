2a41734c4bf29ddab7cafcb83559e9279b4d4bbd
KugaChihiro <c.kuga.24.4.1410@4nonome.com>
2025-04-27 16:05:19 +0900

    index.jsxの訂正

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import MainPage from "./components/Pages/MainPage";
 import "./style.css";
-import { useAlertHandlerContext} from "./contexts/alert_handler";
 import { AlertHandlerProvider } from "./contexts/alert_handler";
-import {AlertManager} from "./components/Organisms/AlertManager";
+import { AlertManager } from "./components/Organisms/AlertManager";
 
-const AlertHandlerContext = useAlertHandlerContext();
-AlertHandlerContext.setAlert
 
 ReactDOM.render(
   <AlertHandlerProvider>
