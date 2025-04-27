1a52677d8e77b839db8ed3b6fa87b3658de084b6
KugaChihiro <c.kuga.24.4.1410@4nonome.com>
2025-04-27 15:17:04 +0900

    index.jsxにおけるAlertManagerの相対パス訂正 -再

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import MainPage from "./components/Pages/MainPage";
 import { useAlertHandlerContext} from "./contexts";
 import "./style.css";
 import { AlertHandlerProvider } from "./contexts";
-import {AlertManager} from "./Organisms/AlertManager";
+import {AlertManager} from "./components/Organisms/AlertManager";
 
 const AlertHandlerContext = useAlertHandlerContext();
 AlertHandlerContext.setAlert
